38997b59df17d143cacdcc11a6fe2f1177164050
volyas <115228295+volyas@users.noreply.github.com>
2023-10-31 22:25:28 +0800

    Что-то типо прототипа функции выбора файла и кнопки для этого.

--- CG2/MainWindow.xaml.cs
+++ CG2/MainWindow.xaml.cs
@@ -88,5 +88,23 @@ namespace CG2
         {
 
         }
+
+        private void OpenFileDialogButton_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new Microsoft.Win32.OpenFileDialog();
+            dialog.FileName = "Document"; // Default file name
+            dialog.DefaultExt = ".txt"; // Default file extension
+            dialog.Filter = "Text documents (.txt)|*.txt"; // Filter files by extension
+
+            // Show open file dialog box
+            bool? result = dialog.ShowDialog();
+
+            // Process open file dialog box results
+            if (result == true)
+            {
+                // Open document
+                string filename = dialog.FileName;
+            }
+        }
     }
 }
