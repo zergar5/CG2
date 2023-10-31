4cb1cf341285fff58a9aa9bffa04449f75ef8343
volyas <115228295+volyas@users.noreply.github.com>
2023-10-31 22:07:18 +0800

    Вписала имена активных элементов и создала каркасы для соответствующих формул выбора.

---
+++ CG2/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace CG2
+{
+    public partial class MainWindow : Window
+    {
+        private void BufferStockCheckBox_Checked(object sender, System.Windows.RoutedEventArgs e)
+        {
+
+        }
+
+        private void DoubleBufferingCheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void LightingMaterialsCheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
