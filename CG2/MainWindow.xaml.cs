dc20172bde19b6f71551799f9854863c40239cd9
volyas <115228295+volyas@users.noreply.github.com>
2023-10-31 23:54:38 +0800

    Добавила функции из Интерфейса

--- CG2/MainWindow.xaml.cs
+++ CG2/MainWindow.xaml.cs
@@ -295,4 +295,103 @@ public partial class MainWindow : Window
 
         texture.Bind(_gl);
     }
+    private void BufferStockCheckBox_Checked(object sender, System.Windows.RoutedEventArgs e)
+    {
+
+    }
+
+    private void DoubleBufferingCheckBox_Checked(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void LightingMaterialsCheckBox_Checked(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void ObjectFrameCheckBox_Checked(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void NormalsCheckBox_Checked(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void TexturesCheckBox_Checked(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void OrthographicProjectionCheckBox_Checked(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void PerspectiveProjectionCheckBox_Checked(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void NormalsSmoothingToggleButton_Checked(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void BufferStockCheckBox_Unchecked(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void DoubleBufferingCheckBox_Unchecked(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void LightingMaterialsCheckBox_Unchecked(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void ObjectFrameCheckBox_Unchecked(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void NormalsCheckBox_Unchecked(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void TexturesCheckBox_Unchecked(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void NormalsSmoothingToggleButton_Unchecked(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void OpenFileDialogButton_Click(object sender, RoutedEventArgs e)
+    {
+        var dialog = new Microsoft.Win32.OpenFileDialog();
+        dialog.FileName = "Document"; // Default file name
+        dialog.DefaultExt = ".txt"; // Default file extension
+        dialog.Filter = "Text documents (.txt)|*.txt"; // Filter files by extension
+
+        // Show open file dialog box
+        bool? result = dialog.ShowDialog();
+
+        // Process open file dialog box results
+        if (result == true)
+        {
+            // Open document
+            string filename = dialog.FileName;
+            FileNameText.Text = filename;
+        }
+
+    }
 }
